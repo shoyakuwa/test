3abaf88f18961a5a0b7b552bba79094936daa89a
shoyakuwa <sho.0529.2@gmail.com>
2016-10-04 15:50:40 +0900

    first commit

---
+++ test/js/custom.js
@@ -0,0 +1,70 @@
+/**
+  * Template Name: Snap Studio
+  * Version: 1.0
+  * Template Scripts
+  * Author: MarkUps
+  * Author URI: http://www.markups.io/
+
+  Custom JS
+
+  1. MENU
+  2. TOSRUS POPUP SLIDER (INTEGRATED PORTFOLIO SECTION)
+
+**/
+
+jQuery(function($){
+
+
+  /* ----------------------------------------------------------- */
+  /*  1. MENU
+  /* ----------------------------------------------------------- */
+
+    jQuery("#nav-icon").click(function(e){
+      e.preventDefault();
+      jQuery("#menu-popup").fadeIn();
+    });
+
+    jQuery("#close-nav").click(function(e){
+      e.preventDefault();
+      jQuery("#menu-popup").fadeOut();
+    });
+
+  /* ----------------------------------------------------------- */
+  /*  2. TOSRUS POPUP SLIDER (INTEGRATED PORTFOLIO SECTION)
+  /* ----------------------------------------------------------- */
+
+  $(".iteam-image1 a").tosrus({
+     pagination : {
+        add        : true
+     },
+     caption    : {
+        add        : true
+     }
+  });
+
+  $(".iteam-image2 a").tosrus({
+       pagination : {
+          add        : true
+       },
+       caption    : {
+          add        : true
+       }
+    });
+  $(".iteam-image3 a").tosrus({
+       pagination : {
+          add        : true
+       },
+       caption    : {
+          add        : true
+       }
+    });
+  $(".iteam-image4 a").tosrus({
+       pagination : {
+          add        : true
+       },
+       caption    : {
+          add        : true
+       }
+    });
+
+});
